251b0c908dbc5537f45f8b251c0dbd6ae0ea575a
StefanButler1984 <46402640+StefanButler1984@users.noreply.github.com>
2019-01-06 15:26:42 -0500

    Update app.js

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ else if (topChild.score < child.score) topChild = topChild
     });
   
   
-$('.stretch').attr('src', topChild.images[0])
+$('.stretch').attr('src', currentNode.images[0])
 });
 
 
