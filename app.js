fb9b8f5ce0db5586163923abd86f8a30e9e0c5f4
StefanButler1984 <46402640+StefanButler1984@users.noreply.github.com>
2019-01-06 15:06:46 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 
+var nodes = [];
+var root = null;
+var topChild = null;
+
+$.get( "https://www.reddit.com/r/Bandersnatch/comments/ad7cbr/bandersnatch_game_engine_test_1/search.json", function( data ) {
+  window.data = data;
+  _.each(a[1].data.children, function(c){
+
+    try{
+      var obj = JSON.parse(c.data.body);
+      obj.replies = c.data.replies.data.children;
+    nodes.push(obj);
+    } catch(ex){}
+    });
+  
+  root = _.find(nodes, function(node){return node.nodeId == 'root'})
+  
+  
+  _.each(root.replies, function(c){
+
+    try{
+        if(topChild==null) topChild = child;
+else if (topChild.score < child.score) topChild = topChild
+      topChild = JSON.parse(topChild.data.body)
+
+    } catch(ex){}
+    });
+  
+  
+
+});
